b2077e373ca33a8541ce599290cc2795eb2496be
Andrei Keida <dronwork@outlook.com>
2018-02-20 19:22:55 +0200

    Правил роуты

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :projects
+  resources :projects do
+    resources :tasks, except: [ :index ]
+  end
+
   devise_for :users
-  root 'home#index'
+  root 'projects#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20180220165843_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.string :status
+      t.references :project, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
