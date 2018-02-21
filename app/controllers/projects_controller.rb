c9f9919c296ea42767455f8eb1f2ee2f545b923f
Andrei Keida <dronwork@outlook.com>
2018-02-21 22:23:19 +0200

    Ассоциации в модели!

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -26,6 +26,7 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(project_params)
 
+
     respond_to do |format|
       if @project.save
         format.html { redirect_to @project, notice: 'Project was successfully created.' }
